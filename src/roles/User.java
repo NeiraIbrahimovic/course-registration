45b0a5fecbfa51951c51a40fb102cce59842a83c
Neira Ibrahimovic <119445388+NeiraIbrahimovic@users.noreply.github.com>
2026-09-21 10:59:29 -0700

    Add recovered source, tests, and portfolio documentation

---
+++ src/roles/User.java
@@ -0,0 +1,73 @@
+package roles;
+/*
+ * The User class is the parent class for all users (student, admin, professor)
+ * This class will be able to get the name, id, user, and password for different user, while keeping them private
+ */
+public abstract class User {
+	private String name;
+	private String id;
+	private String username;
+	private String password;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public User(String id, String name, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+	
+	
+	
+	
+}
